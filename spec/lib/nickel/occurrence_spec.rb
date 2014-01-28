ff7ab74212492adc6688298677580c0375baacc7
Iain Beeston <iain.beeston@gmail.com>
2014-01-28 22:02:40 +0000

    Added occurrence#==
    Need that for testing nlp, which produces occurrences, and I need to be
    able to test if those occurrences are equal to one-another

---
+++ spec/lib/nickel/occurrence_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+require_relative "../../../lib/nickel/occurrence"
+
+describe Nickel::Occurrence do
+  describe "#==" do
+    let(:occ) { Nickel::Occurrence.new(type: :daily, start_date: Nickel::ZDate.new("20140128")) }
+
+    it "is true when comparing to itself" do
+      expect(occ).to eq occ
+    end
+
+    it "is false when comparing to nil" do
+      expect(occ).to_not eq nil
+    end
+
+    it "is false when comparing to a string" do
+      expect(occ).to_not eq "occ"
+    end
+
+    it "is false when comparing to an occurence with different dates" do
+      expect(occ).to_not eq Nickel::Occurrence.new(type: :daily, start_date: Nickel::ZDate.new("20080825"))
+    end
+
+    it "is false when comparing to an occurrence with a different type" do
+      expect(occ).to_not eq Nickel::Occurrence.new(type: :weekly, start_date: Nickel::ZDate.new("20140128"))
+    end
+
+    it "is false when comparing to an occurence with a different interval" do
+      expect(occ).to_not eq Nickel::Occurrence.new(type: :daily, start_date: Nickel::ZDate.new("20140128"), interval: 3)
+    end
+
+    it "is true when comparing to an occurence with the same values" do
+      expect(occ).to eq Nickel::Occurrence.new(type: :daily, start_date: Nickel::ZDate.new("20140128"))
+    end
+  end
+end
