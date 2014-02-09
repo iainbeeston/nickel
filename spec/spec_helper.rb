68a443cedd481fa39d35ab9e3fd34b6e14391f05
Iain Beeston <iain.beeston@gmail.com>
2014-02-09 07:33:21 +0000

    Configured rspec to ignore tests tagged with "broken"

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,5 @@ RSpec.configure do |config|
   config.order = 'random'
   config.expect_with(:rspec){ |c| c.syntax = :expect }
   config.mock_with(:rspec){ |c| c.syntax = :expect }
+  config.filter_run_excluding broken: true
 end
