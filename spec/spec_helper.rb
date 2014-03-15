158a91c9cb28290fc4272c57f6721c4ee2cbfa46
Iain Beeston <iain.beeston@gmail.com>
2014-03-15 11:29:01 +0000

    Made sure we don't print out deprecation warnings when testing deprecated methods
    Makes the test output look noisy and obscures useful output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,15 @@ RSpec.configure do |config|
   config.expect_with(:rspec){ |c| c.syntax = :expect }
   config.mock_with(:rspec){ |c| c.syntax = :expect }
   config.filter_run_excluding broken: true
+
+  # make sure warnings are not shown when testing deprecated methods
+  config.around(:each, :deprecated) do |example|
+    original_verbose = $VERBOSE
+    begin
+      $VERBOSE = nil
+      example.run
+    ensure
+      $VERBOSE = original_verbose
+    end
+  end
 end
