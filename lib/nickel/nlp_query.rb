827efa90a9d26fd614e95aa86ae00f6d3c200f18
Iain Beeston <iain.beeston@gmail.com>
2014-09-30 12:25:37 +0100

    Stopped correcting spelling mistakes
    This is potentially a breaking change, but I think correcting spelling
    mistakes is out of scope for nickel, and will always be done better by
    dedicated software

--- lib/nickel/nlp_query.rb
+++ lib/nickel/nlp_query.rb
@@ -22,8 +22,8 @@ module Nickel
       query_str.downcase!
       remove_unused_punctuation
       replace_backslashes
-      run_spell_check
       remove_unnecessary_words
+      standardize_holidays
       standardize_days
       standardize_months
       standardize_numbers
@@ -78,51 +78,39 @@ module Nickel
       nsub!(/\\/, '/')
     end
 
-    def run_spell_check
-      nsub!(/tomm?orr?ow|romorrow/, 'tomorrow')
-      nsub!(/weeknd/, 'weekend')
-      nsub!(/weekends/, 'every sat sun')
-      nsub!(/everyother/, 'every other')
-      nsub!(/weak/, 'week')
-      nsub!(/everyweek/, 'every week')
-      nsub!(/everymonth/, 'every month')
+    def standardize_holidays
       nsub!(/c?h[oa]nn?[aui][ck][ck]?[ua]h?/, 'hannukkah')
-      nsub!(/frist/, '1st')
-      nsub!(/eveyr|evrey/, 'every')
-      nsub!(/fridya|friady|fridy/, 'friday')
-      nsub!(/thurdsday/, 'thursday')
       nsub!(/x-?mas/, 'christmas')
       nsub!(/st\s+(patrick|patty|pat)s?(\s+day)?/, 'st patricks day')
-      nsub!(/frouth/, 'fourth')
-      nsub!(/\btill\b/, 'through')
-      nsub!(/\bthru\b|\bthrouh\b|\bthough\b|\bthrew\b|\bthrow\b|\bthroug\b|\bthuogh\b/, 'through')
-      nsub!(/weekdays|every\s+weekday/, 'every monday through friday')
-      nsub!(/\bevery?day\b/, 'every day')
-      nsub!(/eigth/, 'eighth')
-      nsub!(/bi[-\s]monthly/, 'bimonthly')
-      nsub!(/tri[-\s]monthly/, 'trimonthly')
     end
 
     def remove_unnecessary_words
       nsub!(/coming/, '')
       nsub!(/o'?clock/, '')
-      nsub!(/\btom\b/, 'tomorrow')
       nsub!(/\s*in\s+(the\s+)?(morning|am)/, ' am')
       nsub!(/\s*in\s+(the\s+)?(afternoon|pm|evenn?ing)/, ' pm')
       nsub!(/\s*at\s+night/, 'pm')
       nsub!(/(after\s*)?noon(ish)?/, '12:00pm')
-      nsub!(/\bmi(dn|nd)ight\b/, '12:00am')
+      nsub!(/\bmidnight\b/, '12:00am')
       nsub!(/final/, 'last')
-      nsub!(/recur(s|r?ing)?/, 'repeats')
+      nsub!(/recur(s|ring)?/, 'repeats')
       nsub!(/\beach\b/, 'every')
       nsub!(/running\s+(until|through)/, 'through')
-      nsub!(/runn?(s|ing)|go(ing|e?s)/, 'for')
+      nsub!(/run(s|ning)/, 'for')
+      nsub!(/go(ing|es)/, 'for')
       nsub!(/next\s+occ?urr?[ae]nce(\s+is)?/, 'start')
-      nsub!(/next\s+date(\s+it)?(\s+occ?urr?s)?(\s+is)?/, 'start')
+      nsub!(/next\s+date(\s+it)?(\s+occurs)?(\s+is)?/, 'start')
       nsub!(/forever/, 'repeats daily')
       nsub!(/\bany(?:\s*)day\b/, 'every day')
       nsub!(/^anytime$/, 'every day')  # user entered anytime by itself, not 'dayname anytime', caught next
       nsub!(/any(\s)?time|whenever/, 'all day')
+      nsub!(/\beveryday\b/, 'every day')
+      nsub!(/\beveryother\b/, 'every other')
+      nsub!(/weekends/, 'every sat sun')
+      nsub!(/\btill\b/, 'through')
+      nsub!(/bi[-\s]monthly/, 'bimonthly')
+      nsub!(/tri[-\s]monthly/, 'trimonthly')
+      nsub!(/weekdays|every\s+weekday/, 'every monday through friday')
     end
 
     def standardize_days
